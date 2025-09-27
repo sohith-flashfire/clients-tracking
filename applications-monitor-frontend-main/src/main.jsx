5449fde8f7630b5c814d3fa164b8af2f94336394
Biswajit Sharma <biswajit@Biswajit.>
2025-09-27 12:35:08 +0000

    frec

--- applications-monitor-frontend-main/src/main.jsx
+++ applications-monitor-frontend-main/src/main.jsx
@@ -54,7 +54,7 @@ const router = createBrowserRouter([
       {
         path:'/email-campaigns/report',
         element : <Report />
-      }
+      },
       {
         path : '/monitor-clients',
         element: <Monitor />
