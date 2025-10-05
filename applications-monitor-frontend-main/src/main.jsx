e055e100717a0c562d11923a32ec4827f1b1a5c4
support1122 <support@flashfirejobs.com>
2025-10-05 09:30:25 +0530

    Update main.jsx

--- applications-monitor-frontend-main/src/main.jsx
+++ applications-monitor-frontend-main/src/main.jsx
@@ -54,6 +54,10 @@ const router = createBrowserRouter([
       {
         path : '/manager-dashboard',
         element: <ManagerDashboard />
+      },
+      {
+        path : '/operations',
+        element: <Monitor />
       }
     ]
   }
